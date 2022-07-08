4aca130c856823b0819761333245167a24e397d3
CPoet <llzero54@foxmail.com>
2022-07-08 09:50:40 +0800

    fix: update

---
+++ client-web/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <markdown-editor />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import MarkdownEditor from "@/components/MarkdownEditor.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    MarkdownEditor,
+  },
+});
+</script>
+
+<style lang="scss"></style>
